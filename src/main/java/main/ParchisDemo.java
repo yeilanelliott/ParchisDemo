9f9e99a60f6fd09b323809de61bd8f890a55666b
yeilanelliott <elliottyeilan@gmail.com>
2025-10-16 19:39:31 -0600

    Primer commit

---
+++ src/main/java/main/ParchisDemo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import view.GUIInicio;
+
+/**
+ *
+ * @author ellio
+ */
+public class ParchisDemo {
+
+    public static void main(String[] args) {
+        //importo la clase GUIInicio
+        GUIInicio v1 = new GUIInicio();
+        //Metodo para visualizar el diiseño de la clase
+        v1.setVisible(true);
+        v1.setLocationRelativeTo(v1);
+    }
+}//fin del main
